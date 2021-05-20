f359a924c2cdb9d033dfe3c27284716ba773c15d
siyuduan6 <74063021+siyuduan6@users.noreply.github.com>
2021-05-20 17:46:49 -0500

    Update zip_code_crawler.py

--- zip_code_crawler.py
+++ zip_code_crawler.py
@@ -11,17 +11,18 @@ async def main():
     """
     # If laptop crashes or crawling is paused by accident, paste the output to "attempt.csv" file
     # and use codes below to restart.
-    """exist = pd.read_csv("attempt.txt", header=None).rename(columns={0: "zip"})
-    gv = pd.read_csv("stage3.csv")
+    """exist = pd.read_csv("attempt.csv", header=None).rename(columns={0: "zip"})
+    gv = pd.read_csv("zip_code_crawler2.csv")
     gv["Full_Address"] = gv["address"] + gv["city_or_county"] + gv["state"]
     gv_il = gv[gv["date"].str.match(r"^2017.*") == True].reset_index()
     address = gv_il["Full_Address"]
     a = exist["zip"].size
     gv_il.loc[:, "zip"] = 0
     gv_il.loc[:a, "zip"] = exist["zip"]"""
-    gv = pd.read_csv("zip_code_crawler.csv", header=0)
+    gv = pd.read_csv("zip_code_crawler1.csv", header=0, low_memory=False)
     gv["Full_Address"] = gv["address"] + gv["city_or_county"] + gv["state"]
     address = gv["Full_Address"]
+    gv["zip"] = ""
     k = 0
     for i in address.to_list():
         if gv["zip"][k] == "":
@@ -36,15 +37,15 @@ async def main():
             else:
                 zip = None
             gv.iloc[k, -1] = zip
-            print(zip, k)
+            print(zip)
         k += 1
         if k % 10000 == 0:
             gv.to_csv("zip_code_crawler2.csv")
-            print(gv)
     gv.to_csv("zip_code_crawler2.csv")
 
 if __name__ == '__main__':
     asyncio.run(main())
+
 
 
 
